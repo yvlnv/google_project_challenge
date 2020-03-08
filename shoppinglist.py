cd5cefbc747a14d96be17dad0121ead929f632ff
Yana <yana@Yanas-iMac.local>
2020-03-08 18:08:57 +0000

    Initial commit

---
+++ shoppinglist.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START imports]
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+
+# We set a parent key on the 'Greetings' to ensure that they are all
+# in the same entity group. Queries across the single entity group
+# will be consistent. However, the write rate should be limited to
+# ~1/second.
+
+# def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+#     """Constructs a Datastore key for a Guestbook entity.
+#     We use guestbook_name as the key.
+#     """
+#     return ndb.Key('Guestbook', guestbook_name)
+
+
+# [START item]
+
+class Item(ndb.Model):
+    """A main model for representing an individual Shopping List item."""
+    content = ndb.StringProperty(indexed=False)
+# [END item]
+
+
+# [START main_page]
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+# [END main_page]
+
+
+# [START app]
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+# [END app]
